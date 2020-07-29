d6bc51a1f1b06bc0439842b3b50b6e7e242e845d
CL-xkay <2869625763@qq.com>
2020-07-30 01:23:47 +0800

    个人信息界面完善

--- src/components/personInfo.vue
+++ src/components/personInfo.vue
@@ -2,32 +2,78 @@
   <div class="homeBox">
     <navigation class="navBox"></navigation>
     <div class="mainBox">
-      <div>
-      <img src="../assets/select.png"></div>
-      <Card style="width:60%" bordered="false" padding="40">
-        <div style="text-align:center">
-          <h3>个人信息</h3>
-          <Form :model="userInfo" label-position="left" :label-width="80">
-            <FormItem label="编号">
-              {{userInfo.id}}
-            </FormItem>
-            <FormItem label="姓名">
-             {{userInfo.name}}
-            </FormItem>
-            <FormItem label="性别">
-              {{userInfo.sex}}
-            </FormItem>
-            <FormItem label="电话">
-              {{userInfo.tel}}
-            </FormItem>
-            <FormItem label="邮箱">
-              {{userInfo.email}}
+      <div class="mainContent">
+        <div style="display: flex;flex-direction: column">
+          <img
+            src="../assets/info.jpg"
+            width="200"
+            height="200"
+            style="border-radius: 50%"
+          />
+          <span style="font-size: 20px;margin-top: 10px;color: #2d8cf0">奥利给!</span>
+        </div>
+        <div class="card" >
+          <Card >
+            <p slot="title" style="font-size: large">
+              <Icon type="md-person" />
+              个人信息
+            </p>
+            <div>
+              <Form :model="userInfo" :label-width="120" class="fontsize" >
+                <FormItem label="编号">
+                  <span style="font-size: 15px">{{ userInfo.id }}</span>
+                </FormItem>
+                <FormItem label="姓名">
+                  <span style="font-size: 15px">{{ userInfo.name }}</span>
+                </FormItem>
+                <FormItem label="性别">
+                  <span style="font-size: 15px">{{ userInfo.sex }}</span>
+                </FormItem>
+                <FormItem label="电话">
+                  <span style="font-size: 15px">{{ userInfo.tel }}</span>
+                  <Button
+                    style="margin-left: 15px"
+                    shape="circle"
+                    type="primary"
+                    icon="ios-create-outline"
+                    @click="telChange"
+                  ></Button>
+                </FormItem>
+                <FormItem label="邮箱">
+                  <span style="font-size: 15px">{{ userInfo.email }}</span>
+                  <Button
+                    style="margin-left: 15px"
+                    shape="circle"
+                    type="primary"
+                    icon="ios-create-outline"
+                    @click="emailChange"
+                  ></Button>
+                </FormItem>
+              </Form>
+            </div>
+          </Card>
+        </div>
+        <Modal v-model="modelTel" @on-ok="telChanged" title="新电话号码">
+          <Form>
+            <FormItem>
+              <Input v-model="newTel" />
             </FormItem>
           </Form>
+        </Modal>
+        <Modal v-model="modelEml" @on-ok="emlChanged" title="新邮箱地址">
+          <Form>
+            <FormItem>
+              <Input v-model="newEmail" />
+            </FormItem>
+          </Form>
+        </Modal>
+        <div class="returnBtn">
+          <Button type="primary" shape="circle" to="/bookSearching">
+            <Icon type="ios-undo" />
+            返回主界面
+          </Button>
         </div>
-      </Card>
-      <div>
-      <router-link to="/bookSearching" tag="Button">返回</router-link></div>
+      </div>
     </div>
   </div>
 </template>
@@ -40,15 +86,33 @@ export default {
   components: {
     navigation
   },
-  data(){
+  data() {
     return {
       userInfo: {
-        id:"0101",
-        name:"Cl",
-        sex:"女",
-        tel:"1234567890",
-        email:"281922222@qq.com"
-      }
+        id: "0101",
+        name: "Cl",
+        sex: "女",
+        tel: "1234567890",
+        email: "281922222@qq.com"
+      },
+      modelTel: false,
+      modelEml: false,
+      newTel: "",
+      newEmail: ""
+    };
+  },
+  methods: {
+    telChange() {
+      this.modelTel = true;
+    },
+    emailChange() {
+      this.modelEml = true;
+    },
+    telChanged() {
+      console.log(this.newTel);
+    },
+    emlChanged() {
+      console.log(this.newEmail);
     }
   }
 };
@@ -67,6 +131,22 @@ export default {
   width: 80%;
   min-height: 66%;
   background-color: white;
+}
+.mainContent {
   display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+.fontsize /deep/ .ivu-form-item-label {
+  font-size: 15px;
+}
+.card {
+  width: 55%;
+  margin-top: 4%;
+}
+.returnBtn {
+  margin-bottom: -26%;
+  margin-right: 3%;
 }
 </style>
